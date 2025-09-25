38eeeb6ba24e44a387576fa673c66ed85ce4bdb7
Christopher F Jensen <chridifj@gmail.com>
2025-09-25 18:17:25 +0200

    Update Delopgave_4.py

--- Delopgave_4/Delopgave_4.py
+++ Delopgave_4/Delopgave_4.py
@@ -11,6 +11,17 @@ Hus_data_df.head(10) #Printer 10 første linjer
 Region_data = Hus_data_df.groupby("region") #Grupperer efter region
 Reg_avg_purchase = Region_data["purchase_price"].mean() # Tager den gennemsnitlige værdi for hver region
 
-House_type_df = Hus_data_df.groupby("house_type")["purchase_price"]
+House_type_df = Hus_data_df.groupby("house_type") # Laver igen en groupby
 
 Hus_data_df.groupby("house_type")["purchase_price"].plot(legend=True)
+# Er igang med at bruge matplotlib igen.
+House_type_df.head(10)
+
+Reg_avg_purchase.plot(kind="bar")
+plt.title("Avg Price by Region")
+plt.xlabel("region")
+plt.xticks(rotation = 45)
+plt.show()
+
+
+#Tilføj plot som viser den gennemsnitlige pris af hver type villa i hver region
